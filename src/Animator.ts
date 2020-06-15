383ed0f84a632403ebc3a85828cdc2cb23502ceb
kwonkyo <victor.kwon1@gmail.com>
2020-06-15 11:31:51 -0600

    Update API documentation

--- src/Animator.ts
+++ src/Animator.ts
@@ -3,9 +3,8 @@ import { IAnimatable } from "./Animatable.js";
 
 
 /**
- * Animators instruct IAnimatables to update their animation state,
- * and then passes the new state to the corresponding IAnimation object
- * which will update the frame.
+ * Animators take the latest animation state from animated models
+ * and pass them on to animatables.
  */
 interface IAnimator<T extends IAnimatedModel, K extends IAnimatable> {
     animate(model: T, animatable: K) : void;
